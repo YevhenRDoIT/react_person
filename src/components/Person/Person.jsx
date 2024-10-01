73963a61060227263a4ed37875fe0a97160de4a9
Yevhen Rudnytskyi <yevhen.dodev@gmail.com>
2024-10-01 10:31:12 +0300

    update solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
-  const personPartnerType = sex === 'f' ? 'wife' : 'husband';
+  const personPartnerType = sex === 'm' ? 'wife' : 'husband';
 
   const personPartner = isMarried
     ? `${partnerName} is my ${personPartnerType}`
@@ -13,7 +13,10 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
       {age && <p className="Person__age">I am {age}</p>}
-      {isMarried ? personPartnerType : personPartner}
+
+      <p className="Person__partner">
+        {isMarried ? `${personPartner}` : 'I am not married'}
+      </p>
     </section>
   );
 };
